29a1e66c58c27b4adca007c336d9fb34fea8e8a2
rjjatson <rjjatson@gmail.com>
2020-03-09 18:14:55 +0700

    implement restful api handler ! untested

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,7 +2,7 @@ package model
 
 // SendNotifRequest is the request model for send notif API
 type SendNotifRequest struct {
-	Message string `json:"message`
+	Message string `json:"message"`
 }
 
 // GetAllNotifResponse is the request model for send notif API
@@ -12,6 +12,6 @@ type GetAllNotifResponse struct {
 
 // Notification is the object model for stored notification
 type Notification struct {
-	Message string `json:"message`
-	Time    string `json:"time`
+	Message string `json:"message"`
+	Time    string `json:"time"`
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"net/http"
+	"time"
 	"warpin/internal/dao"
+	"warpin/internal/model"
 
 	"github.com/emicklei/go-restful"
 )
@@ -13,13 +15,44 @@ type Service struct {
 	inbound  chan []byte
 }
 
-// HandleGetAll is the handler of get all notif API
-func (svc *Service) HandleGetAll(request *restful.Request, response *restful.Response) {
-
-}
-
 // HandleStore is the handler of store notif API
 func (svc *Service) HandleStore(request *restful.Request, response *restful.Response) {
+	reqMessage := new(model.SendNotifRequest)
+	err := request.ReadEntity(&reqMessage)
+	if err != nil {
+		// todo log
+		response.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	err = svc.notifDAO.Store(reqMessage.Message, time.Now().UTC())
+	if err != nil {
+		// todo log
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// todo log
 	response.WriteHeader(http.StatusOK)
+}
+
+// HandleGetAll is the handler of get all notif API
+func (svc *Service) HandleGetAll(request *restful.Request, response *restful.Response) {
+	storedMessages, err := svc.notifDAO.GetAll()
+	if err != nil {
+		// todo log
+		response.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	respMessages := make([]model.Notification, 0)
+
+	for _, sm := range storedMessages {
+		respMessages = append(respMessages, model.Notification{
+			Message: sm.Message,
+			Time:    sm.Time.Format(time.RFC3339),
+		})
+	}
+
+	// todo log
+	response.WriteHeaderAndEntity(http.StatusOK, respMessages)
 }
